957634deee48320f2fdf46b90b5e90af8a074281
kennethslee613 <kennethslee.613@gmail.com>
2018-03-24 15:16:26 -0400

    back button for 4-7-8

--- app/src/main/java/com/example/davidkim/sleepapp/fourseveneight.java
+++ app/src/main/java/com/example/davidkim/sleepapp/fourseveneight.java
@@ -1,7 +1,13 @@
 package com.example.davidkim.sleepapp;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toolbar;
 
 public class fourseveneight extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class fourseveneight extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourseveneight);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //back button
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, GraphicsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
+
+
 }
