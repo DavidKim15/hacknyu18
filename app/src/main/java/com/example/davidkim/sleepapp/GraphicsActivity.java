b35daa80d0a348af906a63d644950faa2ed2bc89
David Kim <dkim11@stevens.edu>
2018-03-24 14:20:46 -0400

    direct 478 to gif

--- app/src/main/java/com/example/davidkim/sleepapp/GraphicsActivity.java
+++ app/src/main/java/com/example/davidkim/sleepapp/GraphicsActivity.java
@@ -1,7 +1,10 @@
 package com.example.davidkim.sleepapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
 
 public class GraphicsActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class GraphicsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphics);
+        ImageButton ib = (ImageButton) this.findViewById(R.id.button478);
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GraphicsActivity.this,fourseveneight.class));
+            }
+        });
     }
 }
