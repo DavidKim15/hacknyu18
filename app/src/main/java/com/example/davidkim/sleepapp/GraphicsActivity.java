ebe94e5d23a2f2113dddf6c7e7c1cb67f3a1a097
David Kim <dkim11@stevens.edu>
2018-03-24 12:11:45 -0400

    created graphics activity page

---
+++ app/src/main/java/com/example/davidkim/sleepapp/GraphicsActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidkim.sleepapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphics);
+    }
+}
