67d9fded3f52de780972a186c0e9e9c825eccfd4
kennethslee613 <kennethslee.613@gmail.com>
2018-03-24 11:40:40 -0400

    changed variable names for drawer

--- app/src/main/java/com/example/davidkim/sleepapp/MainActivity.java
+++ app/src/main/java/com/example/davidkim/sleepapp/MainActivity.java
@@ -80,19 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+        if (id == R.id.nav_graphics) {
+            
+        } else if (id == R.id.nav_videos) {
+
+        } else if (id == R.id.nav_sounds) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
